fe0c76be26b0de19a65b0afb3f68eb56e455052f
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-05-22 22:14:49 -0600

    Add router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import dotenv from 'dotenv';
 import App from './components/App';
@@ -7,5 +8,11 @@ import './index.css';
 
 dotenv.config();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>, 
+  document.getElementById('root')
+);
+
 registerServiceWorker();
