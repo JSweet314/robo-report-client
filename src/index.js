22d9481285bff475fc2ceaa74346e870559a11c3
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-05-23 13:36:43 -0600

    Add redux and sagas architecture
    Add root reducer

--- src/index.js
+++ src/index.js
@@ -2,16 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas';
+import rootReducer from './reducers';
 import dotenv from 'dotenv';
 import App from './components/App';
 import './index.css';
 
 dotenv.config();
 
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+  )
+);
+
+sagaMiddleware.run(rootSaga);
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>, 
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
