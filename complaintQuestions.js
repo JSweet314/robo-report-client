7c5c77cd2faed0ae106cd9a645175d92af829f48
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-05-25 17:20:31 -0600

    Add complaintQs
    Object map in progress

---
+++ complaintQuestions.js
@@ -0,0 +1,90 @@
+export const questions = {
+
+}
+
+/*
+Subject
+  <input[text]>
+Description 
+  <input[text]>
+Did the call/message that you are reporting advertise 
+any type of property, goods, or services?
+<select>
+  Yes
+    Type of Property, Goods, or Services 
+      <input[text]>
+  No
+
+Have you or anyone else in your household done any business with the 
+caller/company within the past 18 months immediately before you received 
+the call/message?
+<select>
+  Yes
+  No
+  Uncertain
+Have you or anyone else in your household made any inquiry or application to the
+caller/company within the 3 months immediately before you received the 
+call/message?
+<select>
+  Yes
+  No
+  Uncertain
+Do you or anyone in your household have a personal relationship with the 
+individual that made the call?
+<select>
+  Yes
+  No
+  Uncertain
+Have you or anyone else in your household or business given the caller/company
+permission to call?
+  <select>
+    Yes
+      Did you give permission to the caller/business to call you in writing?
+      <select>
+        Yes
+        No
+      Date you provided permission to the caller/company to call
+        <input[date]> (MUST BE IN FORMAT 'January, 1, 2018')
+    No
+    Uncertain
+
+Date of Your Issue/Problem 
+  <input[date]>
+Time of Your Issue/Problem 
+  <input[time]>
+Type of Call or Message 
+  <select>
+    Abandoned Calls
+    Live Voice
+    Prerecorded voice
+    Text Message
+Did you receive Caller ID Information?
+  <select>
+    Yes
+      Caller ID Number 
+        <input[text]> (555-555-5555)
+      Caller ID Name 
+        <input[text]>
+    No
+    Don't have caller id
+    Uncertain
+    
+Was the caller's business name provided during the call/message? 
+  <select>
+    Yes
+      Was the business name provided at the beginning of the call/message?
+        <select>
+          Yes
+          No
+      Please provide the name of the advertiser provided during the call
+        <input[text]>
+    No
+    Uncertain
+Provide the advertiser's phone number given during the call.
+  <input[text]> (555-555-5555)
+
+I declare under penalty of perjury that (1) I am over 18 years old, (2) I am 
+authorized to make decisions regarding the telephone number listed below, and 
+(3) the information I have provided today on this Federal Communications 
+Commission electronic form is, to the best of my knowledge, true and correct.
+*/
