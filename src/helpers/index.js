27a0b854fcc704daaef29127ddb71006e05fb1af
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-06-04 14:16:23 -0600

    Add label to data for pie chart and build pie chart component

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,8 +2,10 @@
 
 export const mapFCCDataToStateNumbers = fccData => {
   const counts = fccData.reduce((states, complaint) => {
-    !states[complaint.state] ? states[complaint.state] = 1
-      : states[complaint.state]++;
+    if (complaint.state) {
+      !states[complaint.state] ? states[complaint.state] = 1
+        : states[complaint.state]++;
+    }
     return states;
   }, {});
   return Object.keys(counts)
@@ -17,9 +19,11 @@ export const mapFCCDataToStateNumbers = fccData => {
 
 export const mapFCCDataToTypeOfCall = fccData => {
   const counts = fccData.reduce((types, complaint) => {
-    !types[complaint['type_of_call_or_messge']] ? 
-      types[complaint['type_of_call_or_messge']] = 1
-      : types[complaint['type_of_call_or_messge']]++;
+    if (complaint['type_of_call_or_messge']){
+      !types[complaint['type_of_call_or_messge']] ? 
+        types[complaint['type_of_call_or_messge']] = 1
+        : types[complaint['type_of_call_or_messge']]++;
+    }
     return types;
   }, {});
 
@@ -27,10 +31,12 @@ export const mapFCCDataToTypeOfCall = fccData => {
     .reduce((total, value) => total + value, 0);
 
   return Object.keys(counts)
-    .map(key => ({
-      x: key,
-      y: Math.round(counts[key] * 100 / totalReports),
-      label: 
-        `"${key}": ${counts[key]} ${counts[key] > 1 ? 'reports' : 'report'}`
-    }));
+    .map(key => {
+      const percentage = Math.round(counts[key] * 100 / totalReports);
+      return {
+        x: key,
+        y: percentage,
+        label: `${percentage}%\n${key}`
+      };
+    });
 };
