303476fffec9101c9d5e91b8a4191ae1857885cc
Matthew Renn <rennmatthewp@gmail.com>
2018-06-04 12:51:53 -0600

    Add sign-out functionality to UserAccountContainer Add mapStateToProps test Add basic styles

--- src/containers/UserAccountContainer/index.js
+++ src/containers/UserAccountContainer/index.js
@@ -1,26 +1,73 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import * as actions from '../../actions';
+import './styles.css';
 
-/* eslint-disable no-unused-vars */
-export const UserAccountContainer = props => {
-  return <div>UserAccountContainer</div>;
+export const UserAccountContainer = ({ user, handleOAuthSignOut }) => {
+  return (
+    <div className="user-account-container">
+      <p className="user-account-container__p">
+        <strong>First Name: </strong>
+        {user.firstName}
+      </p>
+      <p className="user-account-container__p">
+        <strong>Last Name: </strong>
+        {user.lastName}
+      </p>
+      <p className="user-account-container__p">
+        <strong>Email: </strong>
+        {user.email}
+      </p>
+      <p className="user-account-container__p">
+        <strong>phone: </strong>
+        {user.phone}
+      </p>
+      <p className="user-account-container__p">
+        <strong>Phone Type: </strong>
+        {user.phoneType}
+      </p>
+      <p className="user-account-container__p">
+        <strong>Phone Location: </strong>
+        {user.phoneLocation}
+      </p>
+      <p className="user-account-container__p">
+        <strong>Address: </strong>
+        {user.address}
+      </p>
+      <p className="user-account-container__p">
+        <strong>State: </strong>
+        {user.state}
+      </p>
+      <p className="user-account-container__p">
+        <strong>City: </strong>
+        {user.city}
+      </p>
+      <p className="user-account-container__p">
+        <strong>Zip Code: </strong>
+        {user.zipcode}
+      </p>
+      <button className="user-account-container__button">
+        Edit My Information
+      </button>
+      <button
+        className="user-account-container__button"
+        onClick={handleOAuthSignOut}
+      >
+        Sign Out
+      </button>
+    </div>
+  );
 };
 
 UserAccountContainer.propTypes = {
   user: PropTypes.object.isRequired,
-  captureUser: PropTypes.func.isRequired
+  handleOAuthSignOut: PropTypes.func.isRequired
 };
 
 export const mapStateToProps = state => ({
   user: state.user
 });
 
-export const mapDispatchToProps = dispatch => ({
-  captureUser: user => dispatch(actions.captureUser(user))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(
+export default connect(mapStateToProps, null)(
   UserAccountContainer
 );
--- src/containers/UserAccountContainer/index.test.js
+++ src/containers/UserAccountContainer/index.test.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { UserAccountContainer } from './';
+import { UserAccountContainer, mapStateToProps } from './';
 
 describe('UserAccountContainer', () => {
   let wrapper;
-  const mockCaptureUser = jest.fn();
+  const mockHandleOAuthSignOut = jest.fn();
 
   beforeEach(() => {
     wrapper = shallow(
-      <UserAccountContainer user={{}} captureUser={mockCaptureUser} />
+      <UserAccountContainer
+        user={{ firstName: 'Jerry' }}
+        handleOAuthSignOut={mockHandleOAuthSignOut}
+      />
     );
   });
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('mapStateToProps', () => {
+    it('should map user from state to props', () => {
+      const mockState = { user: { firstName: 'Jill' } };
+      const mapped = mapStateToProps(mockState);
+
+      expect(mapped).toHaveProperty('user', { firstName: 'Jill' });
+    });
   });
 });
