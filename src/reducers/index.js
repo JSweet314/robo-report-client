e4c822b47cf7d4d893b74edc29fa1a777e46d24e
Jon Sweet <jonathan.h.sweet@gmail.com>
2018-06-13 12:34:11 -0600

    Add reportFilterReducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import userReducer from './userReducer';
 import errorReducer from './errorReducer';
 import complaintsReducer from './complaintsReducer';
 import fccDataReducer from './fccDataReducer';
+import reportVisibilityReducer from './reportVisibilityReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
   error: errorReducer,
   complaints: complaintsReducer,
-  fccData: fccDataReducer
+  fccData: fccDataReducer,
+  reportFilter: reportVisibilityReducer
 });
 
 export default rootReducer;
